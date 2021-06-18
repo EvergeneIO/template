19a280a6bf905d1ba08d288dcc63b12b37e405fb
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-18 14:02:32 +0200

    Update configs.ts

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -5,7 +5,7 @@ dotEnvConfig({ export: true });
 export const configs = {
     port: Number(Deno.env.get("PORT")) || 80,
     host: Deno.env.get("HOST") || "localhost",
-    logLevel: Number(Deno.env.get("LOG_LEVEL")) || 1,
+    logLevel: Number(Deno.env.get("LOG_LEVEL")) ?? 1,
     databaseHost: Deno.env.get("DATABASE_HOST"),
     databasePort: Number(Deno.env.get("DATABASE_PORT")) || 5432,
     databaseUser: Deno.env.get("DATABASE_USER"),
