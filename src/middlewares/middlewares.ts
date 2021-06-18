de86edc06fcd0be563024fe79a339402812528e3
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-18 14:02:20 +0200

    Update middlewares.ts

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -2,3 +2,5 @@ export * from "./timing.middleware.ts";
 export * from "./logger.middleware.ts";
 export * from "./notfound.middleware.ts"
 export * from "./error.middleware.ts";
+export * from "./jwt.middleware.ts";
+export * from "./user_guard.middleware.ts"
