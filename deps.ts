a0b069d7dd3ad352d5363acce40e782e799a35fb
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-18 14:02:45 +0200

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { Application, Router, isHttpError, Status, Context } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+export { Application, Router, isHttpError, Status, Context as OakContext, httpErrors } from "https://deno.land/x/oak@v7.5.0/mod.ts";
 export { config as dotEnvConfig } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 export * from "https://deno.land/std@0.95.0/fmt/colors.ts";
 export { Pool } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
@@ -10,6 +10,8 @@ export type {
     NessieConfig,
     PostgreSQLClientOptions,
 } from "https://deno.land/x/nessie@2.0.0-rc2/mod.ts";
-export { copy, copySync, walk, walkSync } from "https://deno.land/std@0.98.0/fs/mod.ts";
+export { copy, copySync, walk, walkSync, existsSync } from "https://deno.land/std@0.98.0/fs/mod.ts";
 export { format } from "https://deno.land/std@0.96.0/datetime/mod.ts";
 export * as mam from "https://denopkg.com/itohatweb/mammoth@master/mod.ts";
+export { prompt, Input, Checkbox } from "https://deno.land/x/cliffy@v0.19.1/prompt/mod.ts";
+export * as jwt from "https://deno.land/x/djwt@v2.2/mod.ts"
