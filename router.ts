189e2273d823d3465032f9a101e21e4ced2f7b58
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-18 14:02:39 +0200

    Update router.ts

--- router.ts
+++ router.ts
@@ -1,8 +1,19 @@
 import { Router } from "./deps.ts";
 
-import templateRouter from "./src/routes/template.routes.ts"
-import exampleRouter from "./src/routes/example.routes.ts"
+// import templateRouter from "./src/routes/template.routes.ts"
+// import exampleRouter from "./src/routes/example.routes.ts"
+
+//export const routers: string[] = [];
+
+/* for await (const file of Deno.readDir('./src/routes')) {
+    if (!file.isFile) continue;
+    if (file.name === "mod.routes.ts") continue;
+    routers.push(file.name.split('.')[0])
+} */
+
+import * as routers from "./src/routes/mod.routes.ts"
+console.log(routers)
 
 export const router = new Router();
 
-router.use(templateRouter.routes(), exampleRouter.routes())
+for (const route of Object.values(routers)) router.use(route.routes())
