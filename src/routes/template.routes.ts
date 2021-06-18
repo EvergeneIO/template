04e7aa69af9fe95b09466c49d264e8314ce7be0e
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-18 14:02:05 +0200

    Update template.routes.ts

--- src/routes/template.routes.ts
+++ src/routes/template.routes.ts
@@ -1,9 +1,30 @@
-import { Context, Router } from "../../deps.ts";
+import { Router, jwt } from "../../deps.ts";
+import { Context } from "../../types/context.ts";
+import { userGuard } from "../middlewares/user_guard.middleware.ts";
 
-const router = new Router({ prefix: "/test" });
+const privateKey = await Deno.readTextFile("./certs/private.pem")
 
-router.get('/', (ctx: Context) => {
-    ctx.response.body = 'lol2'
+export const router = new Router();
+
+router.get('/', async (ctx: Context) => {
+    const token = await jwt.create({ alg: "RS512", typ: "JWT" }, {
+        username: "Newt",
+        groups: ["Admin", "User", "Tester"],
+        permissions: "92"
+    }, privateKey)
+    ctx.cookies.set("token", token)
+    ctx.response.body = token
+})
+
+router.get("/test", (ctx: Context) => {
+    ctx.response.body = { message: true }
+
+})
+
+router.get("/secret", userGuard("SOME_OTHER"), (ctx: Context) => {
+    ctx.response.body = {
+        username: ctx.state.user?.username
+    }
 })
 
 export default router
