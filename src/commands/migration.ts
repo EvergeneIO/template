07bd363ba9d8156dca33edabc0f2945d3aa33573
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-18 14:02:35 +0200

    Delete migration.ts

--- src/commands/migration.ts
+++
@@ -1,2 +0,0 @@
-import { copySync, format } from "../../deps.ts";
-copySync("./src/templates/migration.ts", `./src/database/migrations/${format(new Date(Date.now()), "yyyyMMddHHmmss")}_${Deno.args[0]}.ts`);
