4e0c85a09f4f86168b61fa0b1b7a335c08f02f5d
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-18 14:02:09 +0200

    Update example.routes.ts

--- src/routes/example.routes.ts
+++ src/routes/example.routes.ts
@@ -1,6 +1,6 @@
 import { Context, Router } from "../../deps.ts";
 
-const router = new Router();
+export const router = new Router();
 
 router.get('/', (ctx: Context) => {
     ctx.response.body = { test: true }
