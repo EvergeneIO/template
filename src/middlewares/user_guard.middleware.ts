74162de03301c1ecb62cc5b720ae2007ae70a4f2
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-06-18 14:02:14 +0200

    Create user_guard.middleware.ts

---
+++ src/middlewares/user_guard.middleware.ts
@@ -0,0 +1,24 @@
+import { httpErrors } from "../../deps.ts";
+import { Context } from "../../types/context.ts";
+import { PermissionStrings } from "../constants/permissions.ts";
+import { validatePermissions } from "../utils/permissions.ts";
+
+/**
+ * Has user permission middleware
+ * checks authorization for context user and user permissions if provided
+ */
+export function userGuard(...permissions: PermissionStrings[]) {
+    return async function (context: Context, next: () => Promise<unknown>) {
+        // If not logged in, throw error
+        if (!context.state.user) {
+            throw new httpErrors.Unauthorized("Unauthorized User");
+        }
+
+        // If permissions specified, then check logged in users permissions
+        if (permissions && !validatePermissions(BigInt(context.state.user.permissions), permissions)) {
+            throw new httpErrors.Forbidden("Missing access");
+        }
+
+        await next();
+    };
+}
