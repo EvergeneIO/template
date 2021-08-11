036ca1103ced6e57146dcb09a001007eecf6b703
NewtTheWolf <NewtTheWolf@users.noreply.github.com>
2021-08-11 15:03:41 +0000

    change: prettier code

--- types/configs.ts
+++ types/configs.ts
@@ -1,11 +1,11 @@
 export interface Configs {
-    port: number,
-    host: string,
-    logLevel: number,
-    databaseHost: string,
-    databasePort: number,
-    databaseUser: string,
-    databasePassword: string,
-    database: string,
-    env: string
+  port: number;
+  host: string;
+  logLevel: number;
+  databaseHost: string;
+  databasePort: number;
+  databaseUser: string;
+  databasePassword: string;
+  database: string;
+  env: string;
 }
