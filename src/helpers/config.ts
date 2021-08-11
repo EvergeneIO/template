e1183aea8faa4d5c41dfd8018ae031135a4e5da8
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-08-11 17:02:39 +0200

    function to check config

---
+++ src/helpers/config.ts
@@ -0,0 +1,8 @@
+import { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
+import { Configs } from "../../types/configs.ts";
+
+export function checkConfig(config: string) {
+    const conf = Deno.env.get(config)
+    if (conf === undefined) throw new Error("something wrong with config")
+    return conf
+}
