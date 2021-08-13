af5a06244d1d8f050693afb04549bcf4a3f47572
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-08-13 10:31:24 +0200

    better folder structure

---
+++ src/routes/web/login.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "../../../deps.ts";
+import { Context } from "../../../types/context.ts";
+
+export const router = new Router({ prefix: "/login" });
+
+router.get("/", (ctx: Context) => {
+  //if (ctx.request.session.user) return ctx.response.redirect('/');
+  ctx.response.body = "yay"
+});
+
+
+export default router;
