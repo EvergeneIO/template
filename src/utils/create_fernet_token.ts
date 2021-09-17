7d2d1ba2d29917418a4633d89861403e54c7e3a6
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2021-09-17 18:07:59 +0200

    fix import

--- src/utils/create_fernet_token.ts
+++ src/utils/create_fernet_token.ts
@@ -1,8 +1,9 @@
-import { fernet } from "../../mod.ts";
-
 /**
  * @param expireTime in ms
  */
+
+import { fernet } from "../constants/token.ts";
+
 // deno-lint-ignore ban-types
 export function createFernetToken(content: {}) {
   return fernet.encode(JSON.stringify(content));
